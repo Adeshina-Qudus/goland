f5823b3ccf9b8cfb7956cbea343dcf3874293efe
Adeshina Qudus <qudusa55@gmail.com>
2024-02-25 07:57:58 +0100

    commit

---
+++ chapter4Deitel/salesCommissionCalculator.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func salesCommission() {
+	var weeklyEarnings float64 = 200
+	var totalEarnings float64
+	fmt.Print("Enter item sold : ")
+	var itemSold float64
+	fmt.Scan(&itemSold)
+	var ninePercent = (9 * itemSold) / 100
+	totalEarnings = ninePercent + weeklyEarnings
+	fmt.Println("Total Earnings is ", totalEarnings)
+}
