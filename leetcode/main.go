1b004848eca99eb9e2df9e3279f85d69deafab87
Adeshina Qudus <qudusa55@gmail.com>
2024-03-08 16:54:38 +0100

    commit

--- leetcode/main.go
+++ leetcode/main.go
@@ -1,6 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
 	result := []int{2, 7, 11, 15}
-	twoSum(result, 9)
+	output := twoSum(result, 9)
+	fmt.Println(output)
 }
--- leetcode/twoSum.go
+++ leetcode/twoSum.go
@@ -4,9 +4,11 @@ func twoSum(input []int, target int) []int {
 	var result []int
 	for getIndex := 0; getIndex < len(input); getIndex++ {
 		for getElement := 1; getElement < len(input); getElement++ {
-			if input[getIndex]+input[getElement] == target {
-				result[0] = input[getIndex]
-				result[1] = input[getElement]
+			firstValue := input[getIndex]
+			secondValue := input[getElement]
+			if firstValue+secondValue == target {
+				result = append(result, firstValue)
+				result = append(result, secondValue)
 			}
 		}
 	}
