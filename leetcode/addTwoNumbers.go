e527e1607cecd8aa1b1d57d909b939e189ad9acc
Adeshina Qudus <qudusa55@gmail.com>
2024-03-11 14:08:12 +0100

    commit

---
+++ leetcode/addTwoNumbers.go
@@ -0,0 +1,13 @@
+package main
+
+func addTwoNumbers(firstList []int, secondList []int) []int {
+	var firstValue int = returnIntValueOf(firstList)
+	var sevcondValue int = returnIntValueOf(secondList)
+}
+
+func returnIntValueOf(list []int) int {
+
+	for count := len(list) - 1; count <= 0; count-- {
+
+	}
+}
