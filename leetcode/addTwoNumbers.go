ca0257201c7dec93925bb9ba8e4f0d7565c36812
Adeshina Qudus <qudusa55@gmail.com>
2024-03-11 14:29:47 +0100

    commit

--- leetcode/addTwoNumbers.go
+++ leetcode/addTwoNumbers.go
@@ -1,13 +1,22 @@
 package main
 
+import "strconv"
+
 func addTwoNumbers(firstList []int, secondList []int) []int {
-	var firstValue int = returnIntValueOf(firstList)
-	var sevcondValue int = returnIntValueOf(secondList)
+	var firstValue string = toString(firstList)
+	var secondValue string = toString(secondList)
+	intFirstValue, _ := strconv.Atoi(firstValue)
+	intSecondValue, _ := strconv.Atoi(secondValue)
+	totalOfNumbers := intFirstValue + intSecondValue
+	result := strconv.Itoa(totalOfNumbers)
+	listOfResult :=
+
 }
 
-func returnIntValueOf(list []int) int {
-
+func toString(list []int) string {
+	var getEachNumber string = ""
 	for count := len(list) - 1; count <= 0; count-- {
-
+		getEachNumber += strconv.Itoa(list[count])
 	}
+	return getEachNumber
 }
