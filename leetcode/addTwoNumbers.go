14d3737c8b9686ef7908862a97b5124e4c548d26
Adeshina Qudus <qudusa55@gmail.com>
2024-03-12 08:58:19 +0100

    commit

--- leetcode/addTwoNumbers.go
+++ leetcode/addTwoNumbers.go
@@ -3,27 +3,27 @@ package main
 import "strconv"
 
 func addTwoNumbers(firstList []int, secondList []int) []int {
-	var firstValue string = toString(firstList)
-	var secondValue string = toString(secondList)
+	var firstValue = toString(firstList)
+	var secondValue = toString(secondList)
 	intFirstValue, _ := strconv.Atoi(firstValue)
 	intSecondValue, _ := strconv.Atoi(secondValue)
 	totalOfNumbers := intFirstValue + intSecondValue
 	result := strconv.Itoa(totalOfNumbers)
-	var listOfResult []int = toList(result)
+	var listOfResult = toList(result)
 	return listOfResult
 }
 
 func toList(result string) []int {
 	var resultList []int
-	for eachElement := len(result) - 1; eachElement <= 0; eachElement-- {
+	for eachElement := len(result) - 1; eachElement >= 0; eachElement-- {
 		resultList[eachElement] = int(result[eachElement])
 	}
 	return resultList
 }
 
 func toString(list []int) string {
-	var getEachNumber string = ""
-	for count := len(list) - 1; count <= 0; count-- {
+	var getEachNumber = ""
+	for count := len(list) - 1; count >= 0; count-- {
 		getEachNumber += strconv.Itoa(list[count])
 	}
 	return getEachNumber
--- leetcode/main.go
+++ leetcode/main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	result := []int{2, 7, 11, 15}
-	output := twoSum(result, 9)
+	listOne := []int{2, 4, 3}
+	listTwo := []int{5, 6, 4}
+	output := addTwoNumbers(listOne, listTwo)
 	fmt.Println(output)
 }
