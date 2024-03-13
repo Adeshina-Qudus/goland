5a3725f60cfdb983bd981fa0506f271d6f1bab0a
Adeshina Qudus <qudusa55@gmail.com>
2024-03-13 10:25:21 +0100

    Customer registration vendor's addProduct

---
+++ leetcode/anagram.go
@@ -0,0 +1,19 @@
+package main
+
+func anagram(firstInput string, secondInput string) bool {
+	counter := 0
+	counterOfDoubleElement := 0
+	for eachElement := 0; eachElement < len(firstInput); eachElement++ {
+		for reArrangeElement := 0; reArrangeElement < len(secondInput); reArrangeElement++ {
+			if firstInput[eachElement] == secondInput[reArrangeElement] {
+				counterOfDoubleElement++
+			}
+		}
+		if counterOfDoubleElement > 1 {
+			counterOfDoubleElement = 1
+		}
+		counter += counterOfDoubleElement
+		counterOfDoubleElement = 0
+	}
+	return counter == len(firstInput)
+}
--- leetcode/main.go
+++ leetcode/main.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	listOne := []int{2, 4, 3}
-	listTwo := []int{5, 6, 4}
-	output := addTwoNumbers(listOne, listTwo)
-	fmt.Println(output)
+	//listOne := []int{2, 4, 3}
+	//listTwo := []int{5, 6, 4}
+	//output := addTwoNumbers(listOne, listTwo)
+	//fmt.Println(output)
+
+	//input := "ayomide"
+	//fmt.Println(strconv.Atoi(strconv.Itoa(int(input[3]))))
+	realvalue := "rat"
+	value := "cat"
+	fmt.Print(anagram(realvalue, value))
 }
